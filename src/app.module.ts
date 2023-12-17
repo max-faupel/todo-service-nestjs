41a891d5a4e8128519cdc6e63b10596d2a440803
Max Faupel <max.faupel@gmail.com>
2023-12-17 10:24:41 +0100

    implement oauth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,19 @@ import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthGuard } from './auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  providers: [
+    AppService,
+    TodoService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users = [];
+
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'max',
+        password: 'dummy',
+      },
+    ];
+  }
+
+  async findOne(username) {
+    return this.users.find((user) => user.username === username);
+  }
+}
