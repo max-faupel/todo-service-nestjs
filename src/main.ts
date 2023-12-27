add72db7d9cc56527b1ef7b0c6c6b647f3a71c19
Max Faupel <max.faupel@gmail.com>
2023-12-27 17:16:33 +0100

    change backend port

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: 'http://localhost:3000' });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3200);
 }
 bootstrap();
